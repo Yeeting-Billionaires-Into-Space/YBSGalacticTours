82fcf49a4cdc89f9a705eff8e9093152cb0bc838
izzy <isabella.wang91@gmail.com>
2021-11-26 22:52:23 -0500

    test api calls to make sure they work, deleted unnecessary files, created component folders

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+
+// imports
+import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import axios from 'axios';
+
+// syntax to import component
+// import Header from './components/Header';
 
 function App() {
+  const [ asteroids, setAsteroids ] = useState({});
+
+  useEffect(() => {
+    // axios call: Asteroids NeoWs
+    axios({
+      // only likes to return one week at a time?
+      url:'https://api.nasa.gov/neo/rest/v1/feed',
+      method: 'GET',
+      responseType: 'json',
+      params:{
+        start_date: '2021-12-03',
+        api_key: 'X4WfjYooUbriBSQODcWwLkcOgGZuUlO2JrirMCZN'
+      }
+    })
+      .then((response) => {
+        //                      with date = array that we will want  👇
+        const asteroidResponse = response.data.near_earth_objects['2021-12-10'];
+        console.log(asteroidResponse)
+        })
+
+
+
+
+
+      // api call: NASA Image and Video Library
+      axios({
+        //                                      👇 test nasa id
+        url: 'https://images-api.nasa.gov/asset/PIA01383',
+        method: 'GET',
+        responseType: 'json'
+      })
+        .then((response) => {
+          // gets image url for specified image (by nasa id)
+          const imagePath = response.data.collection.items[0].href;
+          console.log(imagePath, 'image path');
+        })
+  }, [])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+
+      <Routes>
+
+      </Routes>
+    </Router>
   );
 }
 
