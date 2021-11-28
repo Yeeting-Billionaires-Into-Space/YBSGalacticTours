06e365845badd7b3fd61cd8e33a395595c8577ca
izzy <isabella.wang91@gmail.com>
2021-11-27 22:34:04 -0500

    created promise for api calls in gallery

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import axios from 'axios';
 
 // import components
 import LandingPage from './LandingPage';
-import TravelPage from './components/TravelPage';
 import Button from './components/Button';
 import Footer from './components/Footer'
 
@@ -18,6 +17,7 @@ import OurClients from './components/OurClients';
 import ContactPage from './components/ContactPage';
 import TravelPage from './components/TravelPage';
 import AboutPage from './components/AboutPage';
+import Gallery from './components/Gallery'
 
 function App() {
   const [ asteroids, setAsteroids ] = useState({});
@@ -37,32 +37,32 @@ function App() {
       .then((response) => {
         //                      with date = array that we will want  👇
         const asteroidResponse = response.data.near_earth_objects['2021-12-10'];
-        console.log(asteroidResponse)
+        // console.log(asteroidResponse)
         })
 
 
 
 
 
-      // api call: NASA Image and Video Library
-      axios({
-        //                                      👇 test nasa id
-        url: 'https://images-api.nasa.gov/asset/PIA01383',
-        method: 'GET',
-        responseType: 'json'
-      })
-        .then((response) => {
-          // gets image url for specified image (by nasa id)
-          const imagePath = response.data.collection.items[0].href;
-          console.log(imagePath, 'image path');
-        })
+      // // api call: NASA Image and Video Library
+      // axios({
+      //   //                                      👇 test nasa id
+      //   url: 'https://images-api.nasa.gov/asset/PIA01383',
+      //   method: 'GET',
+      //   responseType: 'json'
+      // })
+      //   .then((response) => {
+      //     // gets image url for specified image (by nasa id)
+      //     const imagePath = response.data.collection.items[0].href;
+      //     console.log(imagePath, 'image path');
+      //   })
   }, [])
 
 
 
   return (
     <Router>
-      <div className="">
+      <Gallery />
 
         <Routes>
           <Route path='/' element={<LandingPage />}/>
@@ -70,19 +70,8 @@ function App() {
           {/* <Route path='/about' element={<AboutPage />}/>
           <Route path='/contact' element={<ContactPage />}/>
           <Route path='/ourclients' element={<OurClients />}/> */}
+          <Route path="travel/:planetID" element={<Gallery />} />
         </Routes>
-          
-      </div>
-
-
-      <Routes>
-    
-        <Route path='/travel' element={<TravelPage />}/>
-        <Route path='/about' element={<AboutPage />}/>
-        <Route path='/contact' element={<ContactPage/>}/>
-        <Route path='/ourclients' element={<OurClients />}/>
-          
-      </Routes>
 
     </Router>
   );
--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,1 +1,54 @@
 // Gallery
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+
+function Gallery() {
+
+  const [ imgRefs, setImgRefs ] = useState([]);
+
+  const mars = [ 'PIA04591', 'PIA05036', 'PIA08431', 'PIA03923' ];
+  // const uranus = [ 'PIA18182', 'PIA00037', 'PIA01488', 'PIA00143' ];
+  // const saturn = [ 'PIA01383', 'PIA06060', 'PIA01957', 'PIA08931' ];
+
+  const planetObj = useParams();
+  const planet = planetObj.planetID;
+  console.log(planet)
+
+
+  // api call: Nasa Image and Video Library
+  useEffect(() => {
+    
+    // maps through the array of chosen planet
+    const requests = mars.map((id) => {
+      return fetch(`https://images-api.nasa.gov/asset/${id}`)
+        .then((response) => {
+          return response.json();
+        });
+    });
+
+
+    Promise.all(requests)
+      .then((jsonData) => {
+
+        // gets the image paths from the data returned
+        const data = jsonData;
+        const imagePaths = data.map((obj) => {
+          return obj.collection.items[0].href;
+        })
+        setImgRefs(imagePaths);
+        console.log(imagePaths);
+      })
+
+  }, [])
+
+
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Gallery;
