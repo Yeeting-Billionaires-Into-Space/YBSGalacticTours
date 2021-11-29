466fba72512f715156522fd237f500bb5a8183cf
SeanSip <sean.sipus@gmail.com>
2021-11-29 16:36:27 -0500

    pushing changes to styling of landingPage & clientPage

--- src/LandingPage/index.js
+++ src/LandingPage/index.js
@@ -8,10 +8,10 @@ import { Link } from 'react-router-dom';
 function LandingPage () {
   return (
   <>
-    <section className='wrapper'>
+    <section className='landingPage wrapper'>
       <div className="landingContainer">
         <img className='landingLogo' src='../.././assets/YBSLogo.png' alt='The logo for YBS Galactic Tours' />
-        <Link to='travel' className='default'>enter <span> space </span>
+        <Link to='travel' className='default landingButton'>enter <span> space </span>
         </Link>
       </div>
     </section>
