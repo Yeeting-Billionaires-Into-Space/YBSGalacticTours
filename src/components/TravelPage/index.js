ad5f133cb309a98f72547b4f54c9d230dbac47bd
kristinmc42 <mccollum.k.j@gmail.com>
2021-11-30 17:48:23 -0500

    created counter with onClick for when tours selected

--- src/components/TravelPage/index.js
+++ src/components/TravelPage/index.js
@@ -3,30 +3,45 @@ import { Link } from "react-router-dom";
 import Header from "../Header";
 import './styles.css'
 import Footer from '../Footer';
+import { useState } from "react/cjs/react.development";
+import DisplayToursLeft from "./DisplayToursLeft";
 
 
 
 function TravelPage() {
+  // inititalize state for counter
+  const [ counter, setCounter ] = useState(0);
+
+
+  const handleCounterClicks = () => {
+    counter++;
+    setCounter(counter);
+    console.log(counter)
+  }
+
+ 
+
   return (
     <>
       <Header />
       <section className='wrapper'>
         <h1>Yeet Billionaires <span>Into Space</span></h1>
         <p>3 free virtual tours per day</p>
+        <DisplayToursLeft toursDone={counter}/>
       </section>
 
       <ul className='toursContainer wrapper'>
         <li className='mars tour'>
           <h2>mars</h2>
-          <Link to='mars' className='default'>Virtual Tour</Link>
+          <Link to='mars' className='default' onClick={handleCounterClicks}>Virtual Tour</Link>
         </li>
         <li className='uranus tour'>
           <h2>uranus</h2>
-          <Link to='uranus' className='default'>Virtual Tour</Link>
+          <Link to='uranus' className='default' onClick={handleCounterClicks}>Virtual Tour</Link>
         </li>
         <li className='saturn tour'>
           <h2>saturn</h2>
-          <Link to='saturn' className='default'>Virtual Tour</Link>
+          <Link to='saturn' className='default' onClick={handleCounterClicks}>Virtual Tour</Link>
         </li>
       </ul>
       <Footer />
