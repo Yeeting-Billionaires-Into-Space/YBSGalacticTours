84b1bb6703d36922d5640a67876437f7412fd670
SeanSip <sean.sipus@gmail.com>
2021-12-01 16:53:26 -0500

    w.i.p. styling contact page further, going to add social media icons

--- src/components/ContactPage/index.js
+++ src/components/ContactPage/index.js
@@ -1,12 +1,78 @@
 // ContactPage
 
+// Style components
+import './styles.css';
+import Header from '../Header';
+import Footer from '../Footer';
+import clientDrEvil from '../../assets/clientDrEvil.jpeg';
+import clientBezos from '../../assets/clientBezos.webp';
+import clientColinZevia from '../../assets/clientColinZevia.png';
+import { Link } from 'react-router-dom';
+
+// Nav components
+import MobileNavigation from '../Header/MobileNavigation';
+
+
 
 function ContactPage() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <section>
+          <Header/>
+          <MobileNavigation />
+        <div className="wrapper">
+          <div className='contactContainer'>
+            <h1>contact us</h1> 
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit deserunt provident iusto quam dolorem rem, ut deleniti porro sapiente delectus.</p>
+              <div className="contactFormContainer">
+                <aside className='contactSideCard'>
+                  <h2>media</h2>
+                </aside>
+                  <form action="registerForm">
+                    <label className="hide" htmlFor="firstName">First Name</label>
+                    <input 
+                      className="formFirst"
+                      placeholder="First Name"
+                      name="firstName"
+                      required/>
+                      <label className="hide" htmlFor="lastName"> Last Name</label>
+                      <input 
+                      className="formLast"
+                      placeholder="Last Name"
+                      name="lastName"
+                      required/>
+                    <label className="hide" htmlFor="email">Email</label>
+                      <input 
+                      type="email"
+                      className="form"
+                      placeholder="Email"
+                      name="email"
+                      required/>
+                    
+                    <label className="hide" htmlFor="message">  Message</label>
+                    <textarea
+                      rows={5}
+                      maxLength={350}
+                      
+                      className="textArea"
+                      placeholder="Message"
+                      name="message"
+                      />
+                      <button 
+                      className="form" type="submit">Register</button>
+                  </form>
+                
+              {/* end of form container */}
+              </div>
+            {/* end of contactSideCard */}
+          </div>
+          {/* end of contactContainer */}
+        </div>
+        {/* end of wrapper  */}
+      </section>  
+      <Footer />
+    </>
   )
 }
 
-export default ContactPage
+export default ContactPage;
